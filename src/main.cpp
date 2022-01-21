8e80751d20a42bbbce2ad45208ecdec690f4c859
symac <web@geobib.fr>
2022-01-21 05:44:23 +0100

    changement écran

--- src/main.cpp
+++ src/main.cpp
@@ -178,12 +178,12 @@
 //#define CH376                          // For CXH376 support (reading files from USB stick)
 //#define SDCARD                         // For SD card support (reading files from SD card)
 // Define (just one) type of display.  See documentation.
-#define BLUETFT                        // Works also for RED TFT 128x160
+//#define BLUETFT                        // Works also for RED TFT 128x160
 //#define OLED1306                     // 64x128 I2C OLED SSD1306
 //#define OLED1309                     // 64x128 I2C OLED SSD1309
 //#define OLED1106                     // 64x128 I2C OLED SH1106
 //#define DUMMYTFT                     // Dummy display
-//#define LCD1602I2C                   // LCD 1602 display with I2C backpack
+#define LCD1602I2C                   // LCD 1602 display with I2C backpack
 //#define LCD2004I2C                   // LCD 2004 display with I2C backpack
 //#define ILI9341                      // ILI9341 240*320
 //#define NEXTION                      // Nextion display. Uses UART 2 (pin 16 and 17)
